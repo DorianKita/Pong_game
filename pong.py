c67aa143bb52c727109df19a9a16c9056741d496
Dorian Kita <dorian.kita90@gmail.com>
2024-12-01 19:26:33 +0100

    turned off animations

--- pong.py
+++ pong.py
@@ -7,10 +7,16 @@ screen = Screen()
 screen.setup(width=800,height=600)
 screen.bgcolor("black")
 screen.title("PONG GAME")
+screen.tracer(0)
 
 paddle = Paddle()
 screen.listen()
 screen.onkey(fun=paddle.move_up, key="Up")
 screen.onkey(fun=paddle.move_down, key="Down")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
